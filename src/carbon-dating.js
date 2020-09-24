72dd9df8adeb7e55cc27d013430f39a1a1285597
ghjkyui56 <ksushashaborkina@mail.ru>
2020-09-25 01:51:55 +0300

    feat: make tast 2

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,21 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || isNaN(sampleActivity) || sampleActivity.trim() === "") {
+    return false;
+  } 
+
+  sampleActivity = parseFloat(sampleActivity);
+
+  if (sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0) {
+    return false
+  }
+
+  let k = 0.693/HALF_LIFE_PERIOD;
+
+  let t = (Math.log(MODERN_ACTIVITY/sampleActivity)/k);
+
+  return t;
+
 };
