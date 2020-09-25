06bf00e8dfaea0e81a7e8d51bf6a6fc6d7b66d1b
ghjkyui56 <ksushashaborkina@mail.ru>
2020-09-26 00:10:29 +0300

    feat: make task 3

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members instanceof Array === false){
+    return false;
+  }
+  let t = 0;
+  for (let i = 0; i<members.length;i++) {
+    if (typeof members[i] !== "string") {
+      t+=1;
+      members[i] = "";
+    } else {
+      members[i] = members[i].trim();
+      members[i] = members[i].substring(0,1);
+      members[i] = members[i].toUpperCase();
+    }
+  }
+
+  if (t === members.length){
+    return false;
+  }
+
+  members = members.sort();
+  return members.join("");
 };
