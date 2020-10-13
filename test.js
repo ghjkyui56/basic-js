b6a838014cdabb5a7d345d66b3304d09231e92f6
ghjkyui56 <ksushashaborkina@mail.ru>
2020-10-13 23:36:20 +0300

    fix: fix test file

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/*require('./test/count-cats.test');
+require('./test/count-cats.test');
 require('./test/carbon-dating.test');
 require('./test/dream-team.test');
 require('./test/extended-repeater.test');
@@ -7,4 +7,4 @@ require('./test/vigenere-cipher.test');
 require('./test/hanoi-tower.test');
 require('./test/transform-array.test');
 require('./test/simple-chain.test');
-require('./test/recursive-depth.test');*/
+require('./test/recursive-depth.test');
