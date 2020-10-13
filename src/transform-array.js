668326172bde5e6a5c953bb0bf6f8d81ba1ec933
ghjkyui56 <ksushashaborkina@mail.ru>
2020-10-13 23:35:14 +0300

    feat: make task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (arr === []){
+    return [];
+  }
+
+  if(!arr instanceof Array){
+    throw Error;  
+  }
+
+  t = [];
+
+
+
+  for (let i = 0; i<arr.length;i++){
+
+    if (arr[i]=== '--discard-next'){
+      
+      if(i !== arr.length - 1) i = i + 1;
+    
+    } else if (arr[i] ==='--discard-prev' && arr[i-2] !== '--discard-next'){
+      if (i>0) t.pop();
+      
+    } else if (arr[i] ==='--double-next'){
+      if(i !== arr.length - 1) t.push(arr[i+1]);
+      
+    } else if (arr[i] ==='--double-prev' && arr[i-2] !== '--discard-next'){
+      if (i>0) t.push(arr[i-1]);
+
+    } else if(arr[i]!=='--discard-prev' && arr[i] !== '--double-prev'){
+      t.push(arr[i]);
+    }
+
+  }
+
+  return t;
 };
+
